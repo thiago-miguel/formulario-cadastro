f2ee144efdac0a14d33bbc04f3eed81c0e79d258
thiago-miguel <thiago.alexandre@outlook.com.br>
2025-12-11 08:36:34 -0300

    Persistencia de dados para todos os campos

--- formulario.js
+++ formulario.js
@@ -1,5 +1,38 @@
 let ultimoCepBuscado = "";
 
+document.addEventListener("DOMContentLoaded", () => {
+    const cepSalvo = localStorage.getItem("cep");
+
+    if (cepSalvo != null) {
+        document.getElementById("cep").value = cepSalvo;
+        document.getElementById("logradouro").value = localStorage.getItem("logradouro");
+        document.getElementById("bairro").value = localStorage.getItem("bairro");
+        document.getElementById("cidade").value = localStorage.getItem("cidade");
+        document.getElementById("estado").value = localStorage.getItem("estado");
+    }
+
+    document.getElementById("nome").value = localStorage.getItem("nome") ?? "";
+    document.getElementById("email").value = localStorage.getItem("email") ?? "";
+    document.getElementById("telefone").value = localStorage.getItem("telefone") ?? "";
+    document.getElementById("idade").value = localStorage.getItem("idade") ?? "";
+    document.getElementById("data-nascimento").value = localStorage.getItem("data-nascimento") ?? "";
+    document.getElementById("tamanho-calçado").value = localStorage.getItem("tamanho-calçado") ?? "";
+    document.getElementById("comida-favorita").value = localStorage.getItem("comida-favorita") ?? "";
+
+});
+
+const campos = [
+  "nome", "email", "telefone", "idade",
+  "data-nascimento", "tamanho-calçado", "comida-favorita"
+];
+
+campos.forEach(id => {
+  const input = document.getElementById(id);
+  input.addEventListener("input", () => {
+    localStorage.setItem(id, input.value);
+  });
+});
+
 function buscarCep() {
     let cep = document.getElementById('cep').value.replace(/\D/g, "");
 
@@ -43,15 +76,3 @@ btnLupa.addEventListener("click", buscarCep);
 const cepInput = document.getElementById("cep");
 cepInput.addEventListener("blur", buscarCep);
 cepInput.addEventListener("input", buscarCep);
-
-document.addEventListener("DOMContentLoaded", () => {
-    const cepSalvo = localStorage.getItem("cep");
-
-    if (cepSalvo != null) {
-        document.getElementById("cep").value = cepSalvo;
-        document.getElementById("logradouro").value = localStorage.getItem("logradouro");
-        document.getElementById("bairro").value = localStorage.getItem("bairro");
-        document.getElementById("cidade").value = localStorage.getItem("cidade");
-        document.getElementById("estado").value = localStorage.getItem("estado");
-    }
-});
