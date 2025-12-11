6482a22fe26de53ee7710eb4d73a1f76ed6b225a
thiago-miguel <thiago.alexandre@outlook.com.br>
2025-12-11 08:17:52 -0300

    Persistência de dados para as informações da API do CEP

--- formulario.js
+++ formulario.js
@@ -12,21 +12,29 @@ function buscarCep() {
     const url = `https://viacep.com.br/ws/${cep}/json/`;
 
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-          if (!data.erro) {
-              document.getElementById('logradouro').value = data.logradouro;
-              document.getElementById('bairro').value = data.bairro;
-              document.getElementById('cidade').value = data.localidade;
-              document.getElementById('estado').value = data.uf;
-          } else {
-              alert('CEP não encontrado.');
-          }
-      })
-      .catch(error => {
-          alert('Erro ao buscar o CEP.');
-          console.error(error);
-      });
+        .then(response => response.json())
+        .then(data => {
+            if (!data.erro) {
+                document.getElementById('logradouro').value = data.logradouro;
+                document.getElementById('bairro').value = data.bairro;
+                document.getElementById('cidade').value = data.localidade;
+                document.getElementById('estado').value = data.uf;
+
+                localStorage.setItem("cep", cep);
+                localStorage.setItem("logradouro", document.getElementById('logradouro').value);
+                localStorage.setItem("bairro", document.getElementById('bairro').value);
+                localStorage.setItem("cidade", document.getElementById('cidade').value);
+                localStorage.setItem("estado", document.getElementById('estado').value);
+
+              
+            } else {
+                alert('CEP não encontrado.');
+            }
+        })
+        .catch(error => {
+            alert('Erro ao buscar o CEP.');
+            console.error(error);
+        });
 }
 
 const btnLupa = document.getElementById("btn-lupa");
@@ -35,3 +43,15 @@ btnLupa.addEventListener("click", buscarCep);
 const cepInput = document.getElementById("cep");
 cepInput.addEventListener("blur", buscarCep);
 cepInput.addEventListener("input", buscarCep);
+
+document.addEventListener("DOMContentLoaded", () => {
+    const cepSalvo = localStorage.getItem("cep");
+
+    if (cepSalvo != null) {
+        document.getElementById("cep").value = cepSalvo;
+        document.getElementById("logradouro").value = localStorage.getItem("logradouro");
+        document.getElementById("bairro").value = localStorage.getItem("bairro");
+        document.getElementById("cidade").value = localStorage.getItem("cidade");
+        document.getElementById("estado").value = localStorage.getItem("estado");
+    }
+});
